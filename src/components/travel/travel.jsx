80c75b809a9e6ca483214d14f30a8f254953b688
jijialian <617619820@qq.com>
2019-07-26 15:08:15 +0800

    commit

---
+++ src/components/travel/travel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./travel.scss";
+
+class Travel extends React.Component{
+	render(){
+		return (
+		    <div>
+                <h5>这是travel</h5>
+                <span>这是travel</span>
+            </div>
+		);
+	}
+}
+
+export default Travel;
